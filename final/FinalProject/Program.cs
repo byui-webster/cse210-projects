9dd6f1e354217b546bdef6c6f43a766d299da73b
byui-webster <michaelafolayan35@gmail.com>
2023-04-05 03:02:07 +0100

    I made some corrections to the main string.

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -3,179 +3,240 @@ using System.Collections.Generic;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        Book book = new Book("Title", "Author", "Format", "FilePath");
-        book.Open();
-    }
-}
-
-// Book class representing a single eBook
-public class Book
-{
-	private string title;
-	private string author;
-	private string format;
-	private string filePath;
-	public Book(string title, string author, string format, string filePath)
+	public static void Main(string[] args)
 	{
-		this.title = title;
-		this.author = author;
-		this.format = format;
-		this.filePath = filePath;
-	}
-
-	public string Title
-	{
-		get
+		Book book = new Book("Title", "Author", "Format", "FilePath");
+		book.Open();
+		Console.WriteLine("Enter a search query:");
+		string query = Console.ReadLine();
+		List<int> results = book.Search(query);
+		book.Close();
+		Console.WriteLine("Do you want to create a bookmark? (Y/N)");
+		string createBookmark = Console.ReadLine();
+		if (createBookmark.ToLower() == "y")
 		{
-			return title;
+			Console.WriteLine("Enter the page number:");
+			int pageNumber = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter the bookmark description:");
+			string description = Console.ReadLine();
+			Bookmark bookmark = new Bookmark(pageNumber, description);
+			bookmark.Create();
+		}
+
+		Console.WriteLine("Do you want to edit a bookmark? (Y/N)");
+		string editBookmark = Console.ReadLine();
+		if (editBookmark.ToLower() == "y")
+		{
+			Console.WriteLine("Enter the bookmark description to edit:");
+			string description = Console.ReadLine();
+			Console.WriteLine("Enter the new bookmark description:");
+			string newDescription = Console.ReadLine();
+			Bookmark bookmark = new Bookmark(0, description);
+			bookmark.Edit(newDescription);
+		}
+
+		Console.WriteLine("Do you want to delete a bookmark? (Y/N)");
+		string deleteBookmark = Console.ReadLine();
+		if (deleteBookmark.ToLower() == "y")
+		{
+			Console.WriteLine("Enter the bookmark description to delete:");
+			string description = Console.ReadLine();
+			Bookmark bookmark = new Bookmark(0, description);
+			bookmark.Delete();
+		}
+
+		Console.WriteLine("Do you want to create a note? (Y/N)");
+		string createNote = Console.ReadLine();
+		if (createNote.ToLower() == "y")
+		{
+			Console.WriteLine("Enter the page number:");
+			int pageNumber = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter the note text:");
+			string text = Console.ReadLine();
+			Note note = new Note(pageNumber, text);
+			note.Create();
+		}
+
+		Console.WriteLine("Do you want to edit a note? (Y/N)");
+		string editNote = Console.ReadLine();
+		if (editNote.ToLower() == "y")
+		{
+			Console.WriteLine("Enter the note text to edit:");
+			string text = Console.ReadLine();
+			Console.WriteLine("Enter the new note text:");
+			string newText = Console.ReadLine();
+			Note note = new Note(0, text);
+			note.Edit(newText);
 		}
 	}
 
-	public string Author
+	// Book class representing a single eBook
+	public class Book
 	{
-		get
+		private string title;
+		private string author;
+		private string format;
+		private string filePath;
+		public Book(string title, string author, string format, string filePath)
 		{
-			return author;
+			this.title = title;
+			this.author = author;
+			this.format = format;
+			this.filePath = filePath;
+		}
+
+		public string Title
+		{
+			get
+			{
+				return title;
+			}
+		}
+
+		public string Author
+		{
+			get
+			{
+				return author;
+			}
+		}
+
+		public void Open()
+		{
+			// Code to open the book
+			Console.WriteLine("Opening book: " + title + " by " + author + " in " + format + " format");
+		}
+
+		public void Close()
+		{
+			// Code to close the book
+			Console.WriteLine("Closing book: " + title);
+		}
+
+		public List<int> Search(string query)
+		{
+			// Code to search within the book and return matching page numbers
+			Console.WriteLine("Searching for '" + query + "' in book: " + title);
+			return new List<int>();
 		}
 	}
 
-	public void Open()
+	// BookSettings class managing the settings for a single book
+	public class BookSettings
 	{
-		// Code to open the book
-		Console.WriteLine("Opening book: " + title);
-	}
-
-	public void Close()
-	{
-		// Code to close the book
-		Console.WriteLine("Closing book: " + title);
-	}
-
-	public List<int> Search(string query)
-	{
-		// Code to search within the book and return matching page numbers
-		Console.WriteLine("Searching for '" + query + "' in book: " + title);
-		return new List<int>();
-	}
-}
-
-// BookSettings class managing the settings for a single book
-public class BookSettings
-{
-	protected int fontSize;
-	protected string fontType;
-	protected string bgColor;
-	public BookSettings(int fontSize, string fontType, string bgColor)
-	{
-		this.fontSize = fontSize;
-		this.fontType = fontType;
-		this.bgColor = bgColor;
-	}
-
-	public int FontSize
-	{
-		get
+		protected int fontSize;
+		protected string fontType;
+		protected string bgColor;
+		public BookSettings(int fontSize, string fontType, string bgColor)
 		{
-			return fontSize;
+			this.fontSize = fontSize;
+			this.fontType = fontType;
+			this.bgColor = bgColor;
+		}
+
+		public int FontSize
+		{
+			get
+			{
+				return fontSize;
+			}
+		}
+
+		public string BgColor
+		{
+			get
+			{
+				return bgColor;
+			}
+
+			set
+			{
+				bgColor = value;
+			}
 		}
 	}
 
-	public string BgColor
+	// FontSettings class for managing font settings
+	class FontSettings : BookSettings
 	{
-		get
+		public FontSettings(int fontSize, string fontType, string bgColor) : base(fontSize, fontType, bgColor)
 		{
-			return bgColor;
 		}
 
-		set
+		public string FontType
 		{
-			bgColor = value;
+			get
+			{
+				return fontType;
+			}
+
+			set
+			{
+				fontType = value;
+			}
 		}
 	}
-}
 
-// FontSettings class for managing font settings
-class FontSettings : BookSettings
-{
-	public FontSettings(int fontSize, string fontType, string bgColor) : base(fontSize, fontType, bgColor)
+	// ColorSettings class for managing background color settings
+	class ColorSettings : BookSettings
 	{
-	}
-
-	public string FontType
-	{
-		get
+		public ColorSettings(int fontSize, string fontType, string bgColor) : base(fontSize, fontType, bgColor)
 		{
-			return fontType;
-		}
-
-		set
-		{
-			fontType = value;
 		}
 	}
-}
 
-// ColorSettings class for managing background color settings
-class ColorSettings : BookSettings
-{
-	public ColorSettings(int fontSize, string fontType, string bgColor) : base(fontSize, fontType, bgColor)
+	// Bookmark class representing a single user bookmark within a book
+	class Bookmark
 	{
-	}
-}
+		private int pageNumber;
+		private string description;
+		public Bookmark(int pageNumber, string description)
+		{
+			this.pageNumber = pageNumber;
+			this.description = description;
+		}
 
-// Bookmark class representing a single user bookmark within a book
-class Bookmark
-{
-	private int pageNumber;
-	private string description;
-	public Bookmark(int pageNumber, string description)
-	{
-		this.pageNumber = pageNumber;
-		this.description = description;
-	}
+		public void Create()
+		{
+			// Code to create a new bookmark
+			Console.WriteLine("Creating bookmark: " + description);
+		}
 
-	public void Create()
-	{
-		// Code to create a new bookmark
-		Console.WriteLine("Creating bookmark: " + description);
-	}
+		public void Edit(string newDescription)
+		{
+			// Code to edit an existing bookmark
+			Console.WriteLine("Editing bookmark: " + description + " -> " + newDescription);
+			description = newDescription;
+		}
 
-	public void Edit(string newDescription)
-	{
-		// Code to edit an existing bookmark
-		Console.WriteLine("Editing bookmark: " + description + " -> " + newDescription);
-		description = newDescription;
+		public void Delete()
+		{
+			// Code to delete an existing bookmark
+			Console.WriteLine("Deleting bookmark: " + description);
+		}
 	}
 
-	public void Delete()
+	// Note class representing a single user note within a book
+	class Note
 	{
-		// Code to delete an existing bookmark
-		Console.WriteLine("Deleting bookmark: " + description);
-	}
-}
+		private int pageNumber;
+		private string text;
+		public Note(int pageNumber, string text)
+		{
+			this.pageNumber = pageNumber;
+			this.text = text;
+		}
 
-// Note class representing a single user note within a book
-class Note
-{
-	private int pageNumber;
-	private string text;
-	public Note(int pageNumber, string text)
-	{
-		this.pageNumber = pageNumber;
-		this.text = text;
-	}
+		public void Create()
+		{
+			// Code to create a new note
+			Console.WriteLine("Creating note: " + text);
+		}
 
-	public void Create()
-	{
-		// Code to create a new note
-		Console.WriteLine("Creating note: " + text);
-	}
-
-	public void Edit(string newText)
-	{
-		// Code to edit an existing note
-		Console.WriteLine("Editing note: " + text + " -> " + newText);
+		public void Edit(string newText)
+		{
+			// Code to edit an existing note
+			Console.WriteLine("Editing note: " + text + " -> " + newText);
+		}
 	}
 }
